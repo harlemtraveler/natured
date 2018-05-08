639f911385cb8772fd62969d81c67fe12cb2eec5
GenayaJames <genayakjames@gmail.com>
2018-05-08 15:56:00 -0400

    change order of tables

--- database/seed.sql
+++ database/seed.sql
@@ -1,11 +1,4 @@
-INSERT INTO products
-  (user_id,name,description,price,category_id,stock)
- VALUES
-  (null,'Red bull parachute','This is a top of the line parachute mad ein the USA.', '1500','3','6'),
-  (null,'Fishermans´rod','This is a heavy duty fishing rod, perfect for heavy fish such as marlin, swordfish, and groupers', '1000','4','22'),
-  (null,'Appalachian tent','This is a reliable camping tent designed for temperate regions', '3000','1','10'),
-  (null,'Nike pro','This shoes are design for jogging lovers.', '100','5','40'),
-  (null,'Hike pro','The perfect backpack to go hicking', '500','2','13')
+\c natureDB
 
 INSERT INTO categories
   (categories)
@@ -14,6 +7,15 @@ VALUES
   ('Hiking'),
   ('Skydiving'),
   ('Fishing'),
-  ('Running')
+  ('Running');
+
+INSERT INTO products
+  (user_id,name,description,price,category_id,stock)
+ VALUES
+  (null,'Red bull parachute','This is a top of the line parachute mad ein the USA.', '1500','3','6'),
+  (null,'Fishermans´rod','This is a heavy duty fishing rod, perfect for heavy fish such as marlin, swordfish, and groupers', '1000','4','22'),
+  (null,'Appalachian tent','This is a reliable camping tent designed for temperate regions', '3000','1','10'),
+  (null,'Nike pro','This shoes are design for jogging lovers.', '100','5','40'),
+  (null,'Hike pro','The perfect backpack to go hicking', '500','2','13');
 
 
