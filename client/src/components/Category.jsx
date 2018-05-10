6a2679a661259714e98f930a5c50cac39aa6c0f1
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-05-10 19:15:15 -0400

    Edit Catagory page layout - in group PM

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -5,11 +5,10 @@ import { Link } from 'react-router-dom';
 class Category extends Component {
   render() {
     return (
-      <div>
-      <Link to={`/${this.props.category}`}>
+
 <div className="flex-item">
+  <Link to={`/${this.props.category}`}>
 <div style={{backgroundImage: `url(${this.props.image})`} } className="category-header">
-</div>
 </div>
 </Link>
 </div>
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
 
 function Nav(props) {
   return(
-    <AppBar className="nav">
-      <ul>
-        <li>ABOUT</li>
-        <li>LOGIN</li>
-        <li>REGISTER</li>
-      </ul>
-      <div className="separator">
+    <div className="nav">
+      <div className="wrapper">
+        <ul>
+          <li>ABOUT</li>
+          <li>LOGIN</li>
+          <li>REGISTER</li>
+        </ul>
+        <div className="logo"><img src="https://i.imgur.com/Sgc4hu9.jpg">
+        </img>
+        </div>
+        <ul>
+          <li className="nav-right space-right">SHOP</li>
+          <li className="nav-right">SELL</li>
+          <li className="nav-right">FAQ</li>
+        </ul>
       </div>
-      <div className="logo"><img src="https://i.imgur.com/Sgc4hu9.jpg">
-      </img>
-      </div>
-      <ul>
-        <li className="nav-right space-right">SHOP</li>
-        <li className="nav-right">SELL</li>
-        <li className="nav-right">FAQ</li>
-      </ul>
-    </AppBar>
+    </div>
   )
 }
 
