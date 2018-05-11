9e5cf4553c96e47618e8729b9db0db28b5695fc9
Ricardo Del Castillo <ricardodelcastillo@ool-ad02050d.dyn.optonline.net>
2018-05-11 10:19:26 -0400

    this will not be used because we are using react to filter the info

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,6 @@
 const productsDb = require('../models/products');
+const productsDb = requiere('../models/categories');
+
 
 function getAllProducts(req, res, next) {
   console.log('getting products');
@@ -11,6 +13,57 @@ function getAllProducts(req, res, next) {
     next(err);
   })
 }
+
+function getAllCampingProducts(req, res, next) {
+  console.log('getting products');
+  productsDb.getAllCamping()
+  .then(data => {
+    res.locals.contents = data;
+    next();
+  })
+  .catch(err => {
+    next(err);
+  })
+}
+
+function getAllHikingProducts(req, res, next) {
+  console.log('getting products');
+  productsDb.getAllHiking()
+  .then(data => {
+    res.locals.contents = data;
+    next();
+  })
+  .catch(err => {
+    next(err);
+  })
+}
+
+function getAllSkydivingProducts(req, res, next) {
+  console.log('getting products');
+  productsDb.getAllSkydiving()
+  .then(data => {
+    res.locals.contents = data;
+    next();
+  })
+  .catch(err => {
+    next(err);
+  })
+}
+
+function getAllFishingProducts(req, res, next) {
+  console.log('getting products');
+  productsDb.getAllFishing()
+  .then(data => {
+    res.locals.contents = data;
+    next();
+  })
+  .catch(err => {
+    next(err);
+  })
+}
+
+
+
 
 function getOneProduct(req, res, next) {
   productsDb.getOneProduct(req.params.id)
