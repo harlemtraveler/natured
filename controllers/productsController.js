23f503ba2c3afd84947b7e57939ddae59344e7d4
Ricardo Del Castillo <ricardodelcastillo@ool-ad02050d.dyn.optonline.net>
2018-05-11 15:22:14 -0400

    form

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,58 +1,9 @@
 const productsDb = require('../models/products');
-const productsDb = requiere('../models/categories');
 
 
 function getAllProducts(req, res, next) {
   console.log('getting products');
   productsDb.getAllProducts()
-  .then(data => {
-    res.locals.contents = data;
-    next();
-  })
-  .catch(err => {
-    next(err);
-  })
-}
-
-function getAllCampingProducts(req, res, next) {
-  console.log('getting products');
-  productsDb.getAllCamping()
-  .then(data => {
-    res.locals.contents = data;
-    next();
-  })
-  .catch(err => {
-    next(err);
-  })
-}
-
-function getAllHikingProducts(req, res, next) {
-  console.log('getting products');
-  productsDb.getAllHiking()
-  .then(data => {
-    res.locals.contents = data;
-    next();
-  })
-  .catch(err => {
-    next(err);
-  })
-}
-
-function getAllSkydivingProducts(req, res, next) {
-  console.log('getting products');
-  productsDb.getAllSkydiving()
-  .then(data => {
-    res.locals.contents = data;
-    next();
-  })
-  .catch(err => {
-    next(err);
-  })
-}
-
-function getAllFishingProducts(req, res, next) {
-  console.log('getting products');
-  productsDb.getAllFishing()
   .then(data => {
     res.locals.contents = data;
     next();
