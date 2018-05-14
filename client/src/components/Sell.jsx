611e5ad56c3eb9be541ff4f7049562eba742fd36
GenayaJames <genayakjames@gmail.com>
2018-05-13 22:16:39 -0400

    initial file structure for profile components

--- client/src/components/Sell.jsx
+++ client/src/components/Sell.jsx
@@ -1,18 +1,112 @@
-import React from 'react';
-import '../App.css';
+import React, { Component } from 'react';
 
-function Sell (props) {
-  return(
-    <div>
-      <p>"SELL YOUR STUFF HERE Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-      mollit anim id est laborum."</p>
-    </div>
-  )
+class ProfileSideMenu extends Component {
+  render() {
+    return(
+      <div className="profile-side-menu-container">
+          <ProfilePicture />
+          <EditableProfileInformationCard
+            isOpen={true}
+          />
+          <div className="button-container">
+            <button></button>
+            <button>Cancel</button>
+          </div>
+      </div>
+    );
+  }
 }
 
-export default Sell;
+class ProfilePicture extends Component {
+  render() {
+    return(
+      <img src={this.props.img_url}></img>
+    );
+  }
+}
+
+class ProfileInformationCardList extends Component {
+  render() {
+    return(
+      <div className="profile-information-card-container">
+        <EditableProfileInformationCard
+          name="John Doe"
+          email="jdoe@mail.com"
+          dob="01/01/1899"
+        />
+      </div>
+    )
+  }
+}
+
+class EditableProfileInformationCard extends Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return(
+        <ProfileInformationCardForm
+          name={this.props.name}
+          email={this.props.email}
+          dob={this.props.dob}
+          img_url={this.props.img_url}
+        />
+      );
+    } else {
+      return(
+        <ProfileInformationCard
+          name={this.props.name}
+          email={this.props.email}
+          dob={this.props.dob}
+        />
+      );
+    }
+  }
+}
+
+class ProfileInformationCard extends Component {
+  render() {
+    return(
+      <div className="profile-information-card-container">
+        <div className="name-container">
+          {this.props.name}
+        </div>
+        <div className="email-container">
+          {this.props.email}
+        </div>
+        <div className="dob-container">
+          {this.props.dob}
+        </div>
+      </div>
+    )
+  }
+}
+
+class ProfileInformationCardForm extends Component {
+  render() {
+    return(
+      <form>
+        <div className="field">
+          <label>Name</label>
+          <input type="text" defaultValue={this.props.name} />
+        </div>
+        <div className="field">
+          <label>Email</label>
+          <input type="text" defaultValue={this.props.email} />
+        </div>
+        <div className="field">
+          <label>Date of Birth</label>
+          <input type="text" defaultValue={this.props.dob} />
+        </div>
+        <div className="field">
+          <label>Image URL</label>
+          <input type="text" defaultValue={this.props.img_url} />
+        </div>
+        <div className="button-container">
+          <button></button>
+          <button>Cancel</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default ProfileSideMenu;
