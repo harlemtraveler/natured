7a2a21441c372fbce74b8b7083e28beaf50e072e
GenayaJames <genayakjames@gmail.com>
2018-05-15 13:58:49 -0400

    update portrait

--- client/src/components/profile/ProfilePicture.jsx
+++ client/src/components/profile/ProfilePicture.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import pic from '../../leaf.png';
 
 function ProfilePicture(props) {
+  const img = {
+    backgroundImage: `url(${pic})`,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+  }
+
   return(
     <div className="portrait-container">
-      <div className="portrait"></div>
+      <div className="portrait" style={img}></div>
     </div>
   );
 }
