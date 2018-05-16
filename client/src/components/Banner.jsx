34ed32f20dcdbca91aa32914221f9a84ecb67e8c
GenayaJames <genayakjames@gmail.com>
2018-05-16 09:01:31 -0400

    continue responsive design and change any a tags to Links

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 function Banner(props) {
   return(
     <div className="banner">
-    <div style={{backgroundImage: `url(${props.image})`}} className="feature-header">
-      <div className="feature-text"><h1>{props.text}</h1></div>
-    </div>
-    <Link to="/all">
-      <div className="all-products">Browse all products</div>
-    </Link>
+      <div style={{backgroundImage: `url(${props.image})`}} className="feature-header">
+        <div className="feature-text"><h1>{props.text}</h1></div>
+      </div>
+      <Link to="/all">
+        <div className="all-products">Browse all products</div>
+      </Link>
     </div>
   )
 }
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import '../App.css';
 import Banner from './Banner';
+import { Link } from 'react-router-dom';
 
 function Contact (props) {
   return(
     <div className="flex-container">
-        <Banner
-          image="https://images.unsplash.com/photo-1446488547543-78c11468449a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb40c62d816950ad87c0c95660aaeaf2&auto=format&fit=crop&w=2549&q=80"
-          text="Contact Us"
-        />
-    <div className="menu-links-padding">
-    <h1>How can we help?</h1>
-    <h2>Frequently Asked Questions</h2>
-    <p>Find the answers to our most commonly asked questions <a href="/here"><span className="link-highlight">here</span></a>.</p>
-    <br/>
-    <br/>
-    <h2>Phone</h2>
-    <h2>1-800-NAT-URED</h2>
-    <p>Nature'd Customer Service Reps are available weekdays from 9AM - 5PM and weekends from 8AM - 4PM.
-    Phone orders are accepted 24 hours a day 7 days a week.</p>
-   </div>
-   </div>
+      <Banner
+        image="https://images.unsplash.com/photo-1446488547543-78c11468449a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb40c62d816950ad87c0c95660aaeaf2&auto=format&fit=crop&w=2549&q=80"
+        text="Contact Us"
+      />
+      <div className="menu-links-padding">
+        <h1>How can we help?</h1>
+        <h2>Frequently Asked Questions</h2>
+        <p>Find the answers to our most commonly asked questions <Link to="/faq"><span className="link-highlight">here</span></Link>.</p>
+        <br/>
+        <br/>
+        <h2>Phone</h2>
+        <h2>1-800-NAT-URED</h2>
+        <p>Nature'd Customer Service Reps are available weekdays from 9AM - 5PM and weekends from 8AM - 4PM.
+        Phone orders are accepted 24 hours a day 7 days a week.</p>
+      </div>
+    </div>
   )
 }
 
